d042b87169a41d25f1c2abc9d09c4c350569691c
kankankankankankan <vip029@me.com>
2023-06-07 14:43:02 +0800

    Update
    增加多个工作表获取；
    增加xls转xlsx自动转换；
    增加识别手机号之间的链接符；
    
    页面小部分调整；

--- excel2phone.py
+++ excel2phone.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, send_file, render_template
-from werkzeug.utils import secure_filename
 import os
 import pandas as pd
 import re
 import zipfile
 import time
 from datetime import datetime
+from flask import Flask, request, send_file, render_template, make_response
+from werkzeug.utils import secure_filename
+import openpyxl
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -19,53 +21,104 @@ def upload_files():
     if not files:
         return '没有选中的文件', 400
     else:
+        current_time = datetime.now().strftime('%Y%m%d%H%M')  # current date and time in 'YYYYMMDDHHMM' format
         filenames = []
         for file in files:
             if file:
-                current_time = datetime.now().strftime('%Y%m%d%H%M')  # current date and time in 'YYYYMMDDHHMM' format
                 filename = current_time + "_" + secure_filename(file.filename)  # prepend the timestamp to the filename
                 filepath = os.path.join('/tmp', filename)
                 file.save(filepath)
-                processed_filepath = process_file(filepath, file.filename)
-                filenames.append(processed_filepath)
-        
-        # Create a zip file
-        zip_filename = "/tmp/" + current_time + "_processed.zip"
-        with zipfile.ZipFile(zip_filename, 'w') as zipf:
-            for filename in filenames:
-                # Add file to the zip file
-                # Second argument is the name of the file in the zip file
-                zipf.write(filename, os.path.basename(filename))
+                if os.path.isdir(filepath):  # If the uploaded file is a directory
+                    process_directory(filepath, filenames)
+                else:
+                    processed_filepath = process_file(filepath, file.filename)
+                    if processed_filepath:
+                        filenames.append(os.path.basename(processed_filepath))
 
-        return send_file(zip_filename, as_attachment=True)
+        num_files = len(filenames)
 
+        if not filenames:
+            return '没有找到符合要求的文件', 400
+
+        return render_template('index.html', filenames=filenames, num_files=num_files)
+
+def process_directory(directory, filenames):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(('.xls', '.xlsx')):
+                filepath = os.path.join(root, file)
+                processed_filepath = process_file(filepath, file)
+                if processed_filepath:
+                    filenames.append(os.path.basename(processed_filepath))
 
 def process_file(filepath, original_filename):
-    df = pd.read_excel(filepath)
-    identifiable_numbers = set()  # use set to avoid duplicates
-    phone_pattern = r"^((13[0-9])|(14([0-1]|[4-9]))|(15([0-3]|[5-9]))|(16(2|[5-7]))|(17[1-9])|(18[0-9])|(19[0|1|3])|(19[5-9]))\d{8}$"
+    try:
+        if original_filename.lower().endswith('.xls'):
+            # Convert .xls to .xlsx
+            filepath_xlsx = os.path.splitext(filepath)[0] + '.xlsx'
+            xls = pd.ExcelFile(filepath, engine='xlrd')
+            with pd.ExcelWriter(filepath_xlsx, engine='openpyxl') as writer:
+                for sheet_name in xls.sheet_names:
+                    df_xls = pd.read_excel(xls, sheet_name=sheet_name)
+                    df_xls.to_excel(writer, sheet_name=sheet_name, index=False)
+            filepath = filepath_xlsx
+            original_filename = os.path.splitext(original_filename)[0] + '.xlsx'
+
+        xlsx = pd.ExcelFile(filepath, engine='openpyxl')
+        identifiable_numbers = set()
+        for sheet_name in xlsx.sheet_names:
+            df = pd.read_excel(xlsx, sheet_name=sheet_name)
+            process_worksheet(df, identifiable_numbers)
+
+        base_filename = os.path.splitext(original_filename)[0]
+        output_filename = os.path.join('/tmp', base_filename + ".txt")
+        with open(output_filename, 'w') as f:
+            for phone_number in identifiable_numbers:
+                f.write(phone_number + '\n')
+        return output_filename
+    except Exception as e:
+        print("Error processing file:", str(e))
+        return None
+
+def process_worksheet(df, identifiable_numbers):
+    phone_pattern = r"(?:13[0-9]|14[0-1,4-9]|15[0-3,5-9]|16[2,5-7]|17[1-9]|18[0-9]|19[0,1,3,5-9])\d{8}"
     email_pattern = r"^([a-zA-Z0-9_.+-]+@(126.com|163.com|wo.cn|189.com|139.com))$"
     for column in df:
         for cell in df[column]:
             if pd.notna(cell):
-                cell_str = str(cell)
-                if re.match(email_pattern, cell_str):
-                    email_split = cell_str.split("@")
-                    email_number = re.search(phone_pattern, email_split[0])
-                    if email_number:
-                        identifiable_numbers.add(email_number.group())
-                else:
-                    number = re.search(phone_pattern, cell_str)
-                    if number:
-                        identifiable_numbers.add(number.group())
-    base_filename = os.path.splitext(original_filename)[0]  # get the original file's base name (no extension)
-    output_filename = filepath.replace(secure_filename(original_filename), base_filename) + ".txt"
-    with open(output_filename, 'w') as f:
-        for phone_number in identifiable_numbers:
-            f.write(phone_number + '\n')
-    return output_filename  # return the name of created file
+                cell_strs = re.split('\n|;|\s', str(cell))
+                cell_strs = [i for s in cell_strs for i in s.split('/')]
+                for cell_str in cell_strs:
+                    if re.match(email_pattern, cell_str):
+                        email_split = cell_str.split("@")
+                        email_numbers = re.findall(phone_pattern, email_split[0])
+                        identifiable_numbers.update(email_numbers)
+                    else:
+                        numbers = re.findall(phone_pattern, cell_str)
+                        identifiable_numbers.update(numbers)
 
+@app.route('/download', methods=['GET'])
+def download_file():
+    current_time = datetime.now().strftime('%Y%m%d%H%M')
+    zip_filename = "/tmp/" + current_time + "_processed.zip"
+    filenames = request.args.getlist("filename")
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for filename in filenames:
+            filepath = os.path.join('/tmp', filename)
+            zipf.write(filepath, os.path.basename(filepath))
 
+    data = BytesIO()
+    with open(zip_filename, 'rb') as f:
+        data.write(f.read())
+    data.seek(0)
+
+    os.remove(zip_filename)
+
+    response = make_response(data.getvalue())
+    response.headers.set('Content-Type', 'application/zip')
+    response.headers.set('Content-Disposition', 'attachment', filename=current_time + "_processed.zip")
+
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
